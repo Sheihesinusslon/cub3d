360456d291e6d1882e5bcd84e28b7afae336f9cf
JuanCunha1 <juan.gucunha@gmail.com>
2026-05-13 21:21:49 +0200

    Replace ft_printf with printf for error handling

--- src/bonus/door_texture_parse_bonus.c
+++ src/bonus/door_texture_parse_bonus.c
@@ -21,13 +21,13 @@ int	parse_door_texture_bonus(t_map *map, char *line)
 	if (ft_strncmp(line, "DO ", 3) != 0 && ft_strncmp(line, "DO\t", 3) != 0)
 		return (0);
 	if (map->door_texture.path)
-		return (ft_printf(ERR_DUP_TEXTURE), -1);
+		return (printf(ERR_DUP_TEXTURE), -1);
 	path = skip_spaces(&line[2]);
 	if (*path == '\0')
-		return (ft_printf(ERR_TEXTURE), -1);
+		return (printf(ERR_TEXTURE), -1);
 	map->door_texture.path = ft_strdup(path);
 	if (!map->door_texture.path)
-		return (ft_printf(ERR_MALLOC), -1);
+		return (printf(ERR_MALLOC), -1);
 	return (1);
 }
 
@@ -40,10 +40,10 @@ static int	is_valid_texture_file(char *path)
 		return (-1);
 	len = ft_strlen(path);
 	if (len < 4 || ft_strncmp(path + len - 4, ".xpm", 4) != 0)
-		return (ft_printf(ERR_TEXTURE_XPM), -1);
+		return (printf(ERR_TEXTURE_XPM), -1);
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return (ft_printf(ERR_TEXTURE_PATH), -1);
+		return (printf(ERR_TEXTURE_PATH), -1);
 	close(fd);
 	return (0);
 }
@@ -53,6 +53,6 @@ int	check_door_texture_bonus(t_map *map)
 	if (!IS_BONUS)
 		return (0);
 	if (!map->door_texture.path)
-		return (ft_printf(ERR_TEXTURE), -1);
+		return (printf(ERR_TEXTURE), -1);
 	return (is_valid_texture_file(map->door_texture.path));
 }
