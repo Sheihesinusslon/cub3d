03ddb22ddb5b3ca20ee52186fda193a46ff2d723
JuanCunha1 <juan.gucunha@gmail.com>
2026-05-26 13:29:40 +0200

    some improvement in readbility

--- include/defines.h
+++ include/defines.h
@@ -31,7 +31,7 @@
 # define CHAR_DOOR_OPEN	'd'
 # define BITS_IN_BYTE	8
 # define MOVE_SPEED		0.03
-# define PLAYER_RADIUS	0.15
+# define PLAYER_RADIUS	0.25
 
 # define ERR_USAGE			"Usage: ./cub3d <map.cub>\n"
 # define ERR_MAP			"Error\nInvalid map file.\n"
